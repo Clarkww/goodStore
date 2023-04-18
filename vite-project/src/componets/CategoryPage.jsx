b4f18ce56aba41ed036cb003753d2af161c05b7b
Clarkww <clarkww95@gmail.com>
2023-04-18 22:42:10 +0100

    .

--- vite-project/src/componets/CategoryPage.jsx
+++ vite-project/src/componets/CategoryPage.jsx
@@ -1,10 +1,52 @@
 import React from 'react'
 
 export default function CategoryPage({data, addToCart}) {
-  return (
-    <div>
-        <h1>Category Page</h1>
 
-    </div>
+  // get category info from url
+  let url = window.location.href
+  let category = url.split('/').pop()
+
+  // get products from category
+
+
+    console.log(data.products[0].mainImage)
+
+  return (
+    <main className='all-prod-page'>
+        <h1>{category}</h1>
+
+        {
+            // map through all products in category
+            data.products.map((product) => {
+                if (product.category === category) {
+                    return (
+                        <div className='prod-page-card'>
+                            <img src={product.mainImage} alt="" />
+
+                            <div className="info-aside">
+
+                            <a className='aside-title' href={`/products/${product.id}`}>{product.name}</a>
+
+                            <p className='aside-price'>£{product.price}</p>
+
+                            <button className='aside-btn' onClick={() => addToCart(product)}>Add to cart</button>
+
+                          </div>
+                        </div>
+                    )
+                }
+            }
+            )
+        }
+
+
+
+
+
+
+
+        
+
+    </main>
   )
 }
